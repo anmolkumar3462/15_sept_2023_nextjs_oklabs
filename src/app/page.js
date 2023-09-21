af9864059234cdf47969fc94fd734a94ec0e0287
username <anmolkumar3462@gmail.com>
2023-09-21 08:34:16 +0530

    nextjs_day_3 commit 1

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          This is Anmol&nbsp;
+          
         </p>
         <div>
           <a
